f83bc42e8d35ec1b5ccc4e15d06eb723784191a0
Brama Udi <bramaudi.al@gmail.com>
2021-09-12 17:21:28 +0700

    feat: add emoji

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ export default function Home({ posts }) {
         <div className="pt-6 pb-8 space-y-2 md:space-y-5">
           <PictureOfMe />
           <h1 className="pt-5 text-3xl text-center font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:leading-14">
-            Brama Udi <Twemoji emoji="honker clown" size="inherit" />
+            Brama Udi <Twemoji emoji="clown-honker" size="inherit" />
           </h1>
           <p className="text-lg leading-7 text-gray-900 dark:text-gray-200">
             <p className="my-4">
@@ -39,8 +39,7 @@ export default function Home({ posts }) {
               yang random hehe.
             </p>
             <p className="my-4">
-              Yah selamat datang di blog pribadi saya{' '}
-              <Twemoji emoji="beaming-face-with-smiling-eyes" />
+              Yah selamat datang di blog pribadi saya <Twemoji emoji="pepe-noice" />
             </p>
           </p>
         </div>
