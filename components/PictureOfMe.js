a766f0cc8d916d41cac9ef3ac8fdcd466660310b
Brama Udi <bramaudi.al@gmail.com>
2021-09-13 14:56:51 +0700

    refactor: load both picture first to smooth trans

--- components/PictureOfMe.js
+++ components/PictureOfMe.js
@@ -1,18 +1,24 @@
 import { useTheme } from 'next-themes'
-import Image from 'next/image'
 import { useEffect, useState } from 'react'
+import Image from 'next/image'
 
 const PictureOfMe = () => {
-  const { theme } = useTheme()
-  const [source, setSource] = useState('/static/images/me-light.jpg')
-  useEffect(() => {
-    theme === 'dark'
-      ? setSource('/static/images/me-dark.jpg')
-      : setSource('/static/images/me-light.jpg')
-  }, [theme])
   return (
     <div className="picture-of-me">
-      <Image width="250" height="250" alt="Brama Udi" title="Me in Blitar" src={source} />
+      <Image
+        width="250"
+        height="250"
+        alt="Brama Udi"
+        title="Me in Blitar"
+        src={'/static/images/me-light.jpg'}
+      />
+      <Image
+        width="250"
+        height="250"
+        alt="Brama Udi"
+        title="Me in Blitar"
+        src={'/static/images/me-dark.jpg'}
+      />
     </div>
   )
 }
